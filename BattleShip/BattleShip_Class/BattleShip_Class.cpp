f5505c10d0dc7d3aff97cf2236127c79668bc169
Jaewoo Woo <gewoose@gmail.com>
2014-07-14 11:19:34 +0900

    RandCout

--- BattleShip/BattleShip_Class/BattleShip_Class.cpp
+++ BattleShip/BattleShip_Class/BattleShip_Class.cpp
@@ -6,6 +6,14 @@
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	char a, b;
+	std::cout << "Alphabet: ";
+	std::cin >> a;
+	std::cout << "Number: ";
+	std::cin >> b;
+	srand(time(NULL));
+	std::string strResults[] = { "HIT", "MISS", "DESTROY" };
+	std::cout << strResults[rand() % (sizeof(strResults) / sizeof(strResults[0]))] << std::endl;
 	return 0;
 }
 
